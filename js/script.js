643b5e0832e6b426eadb3dab3d1596ffa43cff55
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-06-20 10:22:14 +0600

    Portfolio

--- js/script.js
+++ js/script.js
@@ -15,6 +15,22 @@ $(document).ready(function() {
 
 })
 
+// shuttle part
+
+$('.toTop').hide()
+
+        $(window).scroll(function() {
+            if ($(this).scrollTop()) {
+                $('.toTop').fadeIn()
+            } else{
+                $('.toTop').fadeOut()
+            }
+        })
+
+        $('.toTop').click(function() {
+            $("html, body").animate({scrollTop: 0}, 1000)
+        })
+
 
 // script for close nav btn
 
